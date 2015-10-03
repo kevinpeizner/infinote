d7fe789eaffc4804f63acc1f4e70f1853fe621d5
Kevin Peizner <kevin.peizner@gmail.com>
2015-10-03 03:14:15 -0700

    CRUD/REST server example - needs auth

--- infinote/server.py
+++ infinote/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
@@ -17,10 +17,82 @@ tasks = [
   }
 ]
 
+### Helpers ###
+def make_public_task(task):
+  new_task = {}
+  for field in task:
+    if field == 'id':
+      new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+    else:
+      new_task[field] = task[field]
+  return new_task
+
+### Error Handlers ###
+@app.errorhandler(404)
+def not_found(error):
+  return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+  return make_response(jsonify({'error': 'Bad request'}), 400)
+
+### CREATE ###
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+  if not request.json or not 'title' in request.json:
+    abort(400)
+  task = {
+    'id': tasks[-1]['id'] + 1,
+    'title': request.json['title'],
+    'description': request.json.get('description', ""),
+    'done': False
+  }
+  tasks.append(task)
+  return jsonify({'task': make_public_task(task)}), 201
+
+### READ ALL ###
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-  return jsonify({'tasks': tasks})
+  return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
+### READ x ###
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+  task = [task for task in tasks if task['id'] == task_id]
+  if len(task) == 0:
+    abort(404)
+  return jsonify({'task': make_public_task(task[0])})
+
+### UPDATE ###
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+  task = [task for task in tasks if task['id'] == task_id]
+  if len(task) == 0:
+    abort(404)
+  if not request.json:
+    abort(400)
+  if 'title' in request.json and type(request.json['title']) != unicode:
+    abort(400)
+  if 'description' in request.json and type(request.json['description']) is not unicode:
+    abort(400)
+  if 'done' in request.json and type(request.json['done']) is not bool:
+    abort(400)
+  task[0]['title'] = request.json.get('title', task[0]['title'])
+  task[0]['description'] = request.json.get('description', task[0]['description'])
+  task[0]['done'] = request.json.get('done', task[0]['done'])
+  return jsonify({'task': make_public_task(task[0])})
+
+### DELETE ###
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+  task = [task for task in tasks if task['id'] == task_id]
+  if len(task) == 0:
+    abort(404)
+  tasks.remove(task[0])
+  return jsonify({'result': True})
+
+
+### CUSTOM ###
 @app.route('/infinote/api/v1.0/', methods=['POST'])
 def job_kickoff():
   return jsonify({'tasks': tasks})
