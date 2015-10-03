3f0961af51acba4bacedfde89f79f6715f2f493a
Kevin Peizner <kevin.peizner@gmail.com>
2015-10-03 03:54:59 -0700

    Added basic auth -- not secure unless on https connections!

--- infinote/server.py
+++ infinote/server.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth() # Just base64 encodes credentials -- NOT SECURE UNLESS DONE ON HTTPS CONNECTION
 
+
+
+### Data Model ###
+# TODO: need real data to be stored in database
 tasks = [
   {
     'id': 1,
@@ -17,7 +23,9 @@ tasks = [
   }
 ]
 
-### Helpers ###
+
+
+### Helper Functions ###
 def make_public_task(task):
   new_task = {}
   for field in task:
@@ -26,6 +34,8 @@ def make_public_task(task):
     else:
       new_task[field] = task[field]
   return new_task
+
+
 
 ### Error Handlers ###
 @app.errorhandler(404)
@@ -36,8 +46,26 @@ def not_found(error):
 def bad_request(error):
   return make_response(jsonify({'error': 'Bad request'}), 400)
 
-### CREATE ###
+
+
+### AUTHENTICATION ###
+@auth.get_password
+def get_password(username):
+  # TODO use real username/password -- user/pass database?
+  if username == 'miguel':
+    return 'python'
+  return None
+
+@auth.error_handler
+def unauthorized():
+  return make_response(jsonify({'error': 'Unauthorized access'}), 403) # Use 403 instead of 401 to prevent auth pop-ups on client.
+
+
+
+### CRUD ###
+# Create
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@auth.login_required
 def create_task():
   if not request.json or not 'title' in request.json:
     abort(400)
@@ -50,21 +78,25 @@ def create_task():
   tasks.append(task)
   return jsonify({'task': make_public_task(task)}), 201
 
-### READ ALL ###
+# Read All
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
+  print(request.headers)
   return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
-### READ x ###
+# Read x
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@auth.login_required
 def get_task(task_id):
   task = [task for task in tasks if task['id'] == task_id]
   if len(task) == 0:
     abort(404)
   return jsonify({'task': make_public_task(task[0])})
 
-### UPDATE ###
+# Update
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
 def update_task(task_id):
   task = [task for task in tasks if task['id'] == task_id]
   if len(task) == 0:
@@ -82,8 +114,9 @@ def update_task(task_id):
   task[0]['done'] = request.json.get('done', task[0]['done'])
   return jsonify({'task': make_public_task(task[0])})
 
-### DELETE ###
+# Delete
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@auth.login_required
 def delete_task(task_id):
   task = [task for task in tasks if task['id'] == task_id]
   if len(task) == 0:
@@ -92,10 +125,16 @@ def delete_task(task_id):
   return jsonify({'result': True})
 
 
+
 ### CUSTOM ###
 @app.route('/infinote/api/v1.0/', methods=['POST'])
+@auth.login_required
 def job_kickoff():
   return jsonify({'tasks': tasks})
 
+
+
 if __name__ == '__main__':
   app.run(debug=True)
+
+
