d43ea5a153c1c404ac44e7219bfc015326e2c298
Kevin Peizner <kevin.peizner@gmail.com>
2015-11-04 22:35:34 -0800

    AWS config

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 #!/Users/Kevin/Devel/python/.virtualenvs/infinote/bin/python
 from app import infinote
-infinote.run(debug=True, host=infinote.config['HOST'], port=infinote.config['PORT'])
+
+application = infinote
+
+if __name__ == "__main__":
+  infinote.run(debug=True, host=infinote.config['HOST'], port=infinote.config['PORT'])
