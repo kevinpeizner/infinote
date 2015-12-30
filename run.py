e83a61ae9af30e4defdb947325b593db09aa44fb
Kevin Peizner <kevin.peizner@gmail.com>
2015-12-30 14:32:49 -0800

    Revert "AWS config"
    This reverts commit d43ea5a153c1c404ac44e7219bfc015326e2c298.

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
 #!/Users/Kevin/Devel/python/.virtualenvs/infinote/bin/python
 from app import infinote
-
-application = infinote
-
-if __name__ == "__main__":
-  infinote.run(debug=True, host=infinote.config['HOST'], port=infinote.config['PORT'])
+infinote.run(debug=True, host=infinote.config['HOST'], port=infinote.config['PORT'])
