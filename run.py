d6a0f0ca4e9cd7d83f6b511a8d40e28b9a87d6cd
Kevin Peizner <kevin.peizner@gmail.com>
2016-01-10 19:18:56 -0800

    Fixed Shebangs on scripts.

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-#!/Users/Kevin/Devel/python/.virtualenvs/infinote/bin/python
+#!/usr/bin/env python
 from app import infinote
 infinote.run(debug=True, host=infinote.config['HOST'], port=infinote.config['PORT'])
