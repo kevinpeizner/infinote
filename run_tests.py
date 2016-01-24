7e22b435f343f3cd73e997ca2b54faa2fcb6bd24
Kevin Peizner <kevin.peizner@gmail.com>
2016-01-24 09:41:13 -0800

    Split our single test suite into multiple test suites. That way we can test our model (and fail early) before we test api_server. This is needed because some tests in api_server may depend on our model to work.

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,10 @@
 from unittest import TestLoader, TextTestRunner
 
 loader = TestLoader()
-testSuite = loader.discover('.')
 testRunner = TextTestRunner()
-testRunner.run(testSuite)
+modelsTestSuite = loader.discover('.', pattern='test_models.py')
+APITestSuite = loader.discover('.', pattern='test_api_server.py')
+print('\n\tRUNNING MODEL TEST CASES\n')
+testRunner.run(modelsTestSuite)
+print('\n\tRUNNING API TEST CASES\n')
+testRunner.run(APITestSuite)
